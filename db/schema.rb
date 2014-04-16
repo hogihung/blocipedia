70d9cde107cb6f0201c8ee993f338d03dec46c69
John Hogarty <hogihung@gmail.com>
2014-04-15 20:09:12 -0400

    Update to database.yml to fix issue with support of RSpec

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140414023326) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -36,8 +33,8 @@ ActiveRecord::Schema.define(version: 20140414023326) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wikis", force: true do |t|
     t.string   "title"
@@ -47,6 +44,6 @@ ActiveRecord::Schema.define(version: 20140414023326) do
     t.integer  "user_id"
   end
 
-  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id", using: :btree
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
