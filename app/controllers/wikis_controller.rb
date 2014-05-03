9fcd1339a1c838e467b83fdf8597daace978d3e5
John Hogarty <hogihung@gmail.com>
2014-05-03 10:35:53 -0400

    Add delete action for wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -42,6 +42,14 @@ class WikisController < ApplicationController
   end
 
   def destroy
-    #...
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+    if @wiki.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error deleting the wiki."
+      render :show
+    end
   end
 end
