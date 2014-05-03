3778e80871fd4636f5dbfb31921f531637d6035d
John Hogarty <hogihung@gmail.com>
2014-05-03 13:13:38 -0400

    Update before filter to include show action in exception.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,5 +1,5 @@
 class WikisController < ApplicationController
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
     if current_user
