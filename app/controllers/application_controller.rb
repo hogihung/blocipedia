24d77250152faf7b268062a09479da3cd906e48d
John Hogarty <hogihung@gmail.com>
2014-05-04 17:06:26 -0400

    Create better exception for private wikis by users who are not authorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
-    redirect_to root_url, alert: exception.message
-  end
+  rescue_from Pundit::NotAuthorizedError, with: :permission_denied
+
 
   protected
 
@@ -15,6 +14,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     wikis_path
+  end
+
+  def permission_denied
+    render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
   end
 
 end
