b76262a1c30985626fb0b5e23972af5a9a52aa72
John Hogarty <hogihung@gmail.com>
2014-05-05 19:20:53 -0400

    Update user model for wiki collaboration.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :wikis
+  has_many :wiki_collaborators
+  has_many :wikis, :through => :wiki_collaborators
 
 end
