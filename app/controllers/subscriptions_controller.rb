fec002a9f463f4f64e0aae1e37c3903bdf9173a6
John Hogarty <hogihung@gmail.com>
2014-05-10 11:41:08 -0400

    Apply policy to new action, and sketch out destroy action for Subscriptions.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,7 @@
 class SubscriptionsController < ApplicationController
   def new
     @subscription = Subscription.new
+    authorize @subscription
   end
 
   def create
@@ -10,9 +11,21 @@ class SubscriptionsController < ApplicationController
       flash[:notice] = "Sign up for Premium account successfull."
       redirect_to edit_user_registration_path
     rescue Exception => e
-      flash[:alert] = "You loose!"
+      flash[:alert] = "There was a problem upgrading to a Premium membership.  Please try again."
       render :new
       puts e.message
+    end
+  end
+
+  def destroy
+    @subscription = Subscription.find(params[:user_id])
+    authorize @subscription
+    if @subscription.destroy
+      flash[:notice] = "Premium membership has been cancelled."
+      redirect_to edit_user_registration_path
+    else
+      flash[:alert] = "There was an error cancelling your Premium membership."
+      redirect_to edit_user_registration_path
     end
   end
 end
