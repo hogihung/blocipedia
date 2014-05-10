1c685eb0fefdc5b45a9adc6e983338abeeecb0e0
John Hogarty <hogihung@gmail.com>
2014-05-10 11:40:04 -0400

    Add tests to visiting users trying to upgrade and cancel subscriptions.

--- spec/features/user_subscription_spec.rb
+++ spec/features/user_subscription_spec.rb
@@ -8,15 +8,15 @@ feature "User upgrades to premium subscription" do
   given(:user) { FactoryGirl.create(:user)}
   given(:premium_user) { FactoryGirl.create(:user, :premium) }
 
-  scenario "A logged in member user should be able to upgrade", :focus, :js do
+  scenario "A logged in member user should be able to upgrade", :js do
     login(user)
     click_link user.name
 
     expect(page).to have_link("Upgrade")
     click_link("Upgrade")
 
-    expect(page).to have_button("Pay with Card")
-    click_button("Pay with Card")
+    expect(page).to have_button("Subscribe")
+    click_button("Subscribe")
 
     stripe = page.driver.window_handles.last
     page.within_window stripe do
@@ -25,13 +25,18 @@ feature "User upgrades to premium subscription" do
       fill_in "card_number", with: "4012 8888 8888 1881"
       fill_in "cc-exp", with: "0614"
       fill_in "cc-csc", with: "123"
-      click_button("Pay $9.00")
+      click_button("Subscribe $9.00")
     end
 
     sleep 1 #iframe wait
     expect(page).to have_content "Sign up for Premium account successfull."
 
     expect(page).to have_link("Cancel Subscription")
+  end
+
+  scenario "A visiting user should not be able to upgrade another users subscription." do
+    visit "http://localhost:3000/users/1/subscription/new"
+    expect(page).to have_content("You are not authorized to perform this action.")
   end
 end
 
@@ -40,6 +45,12 @@ feature "User cancels premium subscription" do
 
   pending "Previous test(s) pass." do
     #...
+  end
+
+  scenario "A visiting user should not be able to cancel another user subscription." do
+    visit "http://localhost:3000/users/1/subscription/"
+    #how do I simulate a destroy via url here? Is it applicable?
+    expect(page).to have_content("You are not authorized to perform this action.")
   end
 
 end
